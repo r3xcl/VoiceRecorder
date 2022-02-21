49df26facb8366e7d806c8424c68938ce8024f45
Matthew Pervykh <pervih.matvey@gmail.com>
2022-02-21 21:49:09 +0200

    Add check record permission, voids start/stop recording

--- app/src/main/java/com/r3xcl/voicerecorder/RecordFragment.java
+++ app/src/main/java/com/r3xcl/voicerecorder/RecordFragment.java
@@ -1,10 +1,16 @@
 package com.r3xcl.voicerecorder;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,12 +20,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.security.Permission;
+
 
 public class RecordFragment extends Fragment implements View.OnClickListener {
 
     private NavController navController;
+
     private ImageView btn_list, btn_record;
+
     private boolean isRecording = false;
+
+    private String recPermission = Manifest.permission.RECORD_AUDIO;
+    private int codePermission = 21;
+
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
 
     public RecordFragment() {
 
@@ -46,25 +63,69 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
         btn_record.setOnClickListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onClick(View view) {
+
         switch (view.getId()){
+
             case R.id.btn_record_list:
                 navController.navigate(R.id.action_recordFragment_to_audioListFragment);
                 break;
             case R.id.btn_record:
                 if(isRecording){
                     //Stop
+                    stopRecording();
+
                     btn_record.setImageDrawable(getResources().getDrawable(R.drawable.ic_dis_rec, null));
                     isRecording = false;
                 }else{
                     //Start
-                    btn_record.setImageDrawable(getResources().getDrawable(R.drawable.ic_en_rec, null));
-                    isRecording = true;
+                    startRecording();
+                    
+                    if(checkPermissions()){
+                        btn_record.setImageDrawable(getResources().getDrawable(R.drawable.ic_en_rec, null));
+                        isRecording = true;
+                    }
                 }
                 break;
 
+        }
+    }
+
+    private void startRecording() {
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        recordFile = "filename.3gp";
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordPath);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private boolean checkPermissions() {
+        if(ActivityCompat.checkSelfPermission(getContext(), recPermission) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recPermission}, codePermission);
+            return false;
         }
     }
 }
