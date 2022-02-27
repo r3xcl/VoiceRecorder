6cb2e0d5797ee39b7d7c98e5e1a1467c9d8f60af
Matthew Pervykh <pervih.matvey@gmail.com>
2022-02-27 19:23:55 +0200

    Add nav methods on media player

--- app/src/main/java/com/r3xcl/voicerecorder/AudioListFragment.java
+++ app/src/main/java/com/r3xcl/voicerecorder/AudioListFragment.java
@@ -1,25 +1,33 @@
 package com.r3xcl.voicerecorder;
 
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
 
-public class AudioListFragment extends Fragment {
+public class AudioListFragment extends Fragment implements AudioListAdapter.onItemListClick{
 
     private ConstraintLayout player_sheet;
 
@@ -31,6 +39,17 @@ public class AudioListFragment extends Fragment {
 
     private AudioListAdapter audioListAdapter;
 
+    private MediaPlayer mediaPlayer = null;
+    private boolean isPlaying = false;
+
+    private File fileToPlay;
+
+    private ImageView btn_play;
+    private TextView playerHeader, playerFileName;
+
+    private SeekBar playerSeek;
+    private Handler seekbarHandler;
+    private Runnable updateSeekbar;
 
     public AudioListFragment() {
 
@@ -58,11 +77,16 @@ public class AudioListFragment extends Fragment {
         bottomSheetBehavior = BottomSheetBehavior.from(player_sheet);
         audioList = view.findViewById(R.id.audio_list_view);
 
+        playerFileName = view.findViewById(R.id.player_file_name);
+        playerHeader = view.findViewById(R.id.player_header);
+        btn_play = view.findViewById(R.id.btn_play);
+        playerSeek = view.findViewById(R.id.player_seekbar);
+
         String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
         File directory = new File(path);
         allFiles = directory.listFiles();
 
-        audioListAdapter = new AudioListAdapter(allFiles);
+        audioListAdapter = new AudioListAdapter(allFiles, this);
 
         audioList.setHasFixedSize(true);
         audioList.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -81,5 +105,140 @@ public class AudioListFragment extends Fragment {
 
             }
         });
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                if(isPlaying){
+                    pauseAudio();
+                }else{
+                    if(fileToPlay != null){
+                        resumeAudio();
+                    }
+                }
+            }
+        });
+
+        playerSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if(fileToPlay != null){
+                    pauseAudio();
+                }
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(fileToPlay != null){
+                    int progress = seekBar.getProgress();
+                    mediaPlayer.seekTo(progress);
+                    resumeAudio();
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClickListener(File file, int position) {
+        fileToPlay = file;
+        if(isPlaying){
+            stopAudio();
+            playAudio(fileToPlay);
+        }else{
+            playAudio(fileToPlay);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void pauseAudio(){
+        mediaPlayer.pause();
+        btn_play.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.ic_play, null));
+        isPlaying = false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void resumeAudio(){
+        mediaPlayer.start();
+        btn_play.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.ic_pause, null));
+        isPlaying = true;
+        seekbarHandler.removeCallbacks(updateSeekbar);
+        updateRunnable();
+        seekbarHandler.postDelayed(updateSeekbar, 0);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void stopAudio() {
+        btn_play.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.ic_play, null));
+        playerHeader.setText("Не проигрывается");
+        isPlaying = false;
+
+        mediaPlayer.stop();
+        seekbarHandler.removeCallbacks(updateSeekbar);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void playAudio(File fileToPlay) {
+
+        mediaPlayer = new MediaPlayer();
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+        try {
+            mediaPlayer.setDataSource(fileToPlay.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        btn_play.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.ic_pause, null));
+        playerFileName.setText(fileToPlay.getName());
+        playerHeader.setText("Проигрывается");
+
+        isPlaying = true;
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopAudio();
+                playerHeader.setText("Проиграно");
+            }
+        });
+
+        playerSeek.setMax(mediaPlayer.getDuration());
+        seekbarHandler = new Handler();
+        updateRunnable();
+
+        seekbarHandler.postDelayed(updateSeekbar, 0);
+    }
+
+    private void updateRunnable() {
+        updateSeekbar = new Runnable() {
+            @Override
+            public void run() {
+                playerSeek.setProgress(mediaPlayer.getCurrentPosition());
+                seekbarHandler.postDelayed(this, 500);
+            }
+        };
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if(isPlaying){
+            stopAudio();
+        }
     }
 }
